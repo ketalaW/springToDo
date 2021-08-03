595aad742b258044ae641e21d5114d0569b799ab
Michał Walatek <michal.walatek@almar.pl>
2021-08-03 19:05:48 +0200

    Przejście na RestController.

--- src/main/java/com/toDo/demo/controller/TaskController.java
+++ src/main/java/com/toDo/demo/controller/TaskController.java
@@ -1,29 +1,36 @@
 package com.toDo.demo.controller;
 
 
+import com.toDo.demo.model.Task;
+import com.toDo.demo.model.SqlTaskRepository;
 import com.toDo.demo.model.TaskRepository;
-import org.hibernate.mapping.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@RepositoryRestController
+import java.util.List;
+
+@RestController
 public class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
-
     private final TaskRepository repository;
 
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/tasks")
-    ResponseEntity<?> readAllTasks() {
-        logger.warn("Pobrano wszystkie taski");
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Soprano wszystkie zadania");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.warn("Read pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 }
